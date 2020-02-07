60193e428d61c614dee929ccc62c63a798a88da3
Matias Moreira <moreiramatias10@hotmail.com>
2020-02-07 02:54:05 -0300

    add driver class and  Vehicle Controller

---
+++ Controllers/VehicleController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        // GET: api/Vehicle
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Vehicle/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Vehicle
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Vehicle/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Driver.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace API_CRUD
+{
+    public class Driver
+    {
+        public int id { get; set; }
+        public String firstname { get; set; }
+
+        public String lastname { get; set; }
+        
+        public string phone { get; set; }
+        public String email { get; set; }
+
+    }
+}
